da7590d3b2eab2918e172fd8cbc39170afb5bf55
Ben51Degrees <ben@51degrees.com>
2025-06-11 17:24:29 +0100

    BUG/TEST: The case of a negative double less than 1 is now handled when converting WKB to string, and a test added. Previously, the int part would have no sign (as it was zero), but the negative was removed from the decimal part.

--- stringBuilder.c
+++ stringBuilder.c
@@ -139,7 +139,7 @@ StringBuilder* fiftyoneDegreesStringBuilderAddDouble(
 	fiftyoneDegreesStringBuilder * const builder,
 	const double value,
 	const uint8_t decimalPlaces) {
-
+	bool addNegative = FALSE;
 	const int digitPlaces = MAX_DOUBLE_DECIMAL_PLACES < decimalPlaces
 		? MAX_DOUBLE_DECIMAL_PLACES : decimalPlaces;
 	int remDigits = digitPlaces;
@@ -148,6 +148,12 @@ StringBuilder* fiftyoneDegreesStringBuilderAddDouble(
 	double fracPart = value - intPart;
 
 	if (fracPart < 0) {
+		if (intPart == 0) {
+			// Handle negative numbers <1. The integer part will just be zero,
+			// which is neither positive or negative. So the negative must be
+			// added.
+			addNegative = TRUE;
+		}
 		fracPart = -fracPart;
 	}
 	if (remDigits <= 0 && fracPart >= 0.5) {
@@ -208,7 +214,9 @@ StringBuilder* fiftyoneDegreesStringBuilderAddDouble(
 	for (; nextDigit >= digits; --nextDigit) {
 		*nextDigit += '0';
 	}
-
+	if (addNegative) {
+		StringBuilderAddChar(builder, '-');
+	}
 	StringBuilderAddInteger(builder, intPart);
 	StringBuilderAddChars(builder, floatTail, digitsToAdd + 1);
 	return builder;
